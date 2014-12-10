864a7ac83614e039b32f26a5f46903f60e235b67
aousabdo <aousabdo@gmail.com>
2014-12-10 17:00:47 -0500

    modified file

--- predictText.R
+++ predictText.R
@@ -1,15 +1,16 @@
+source("cleanStrings.R")
 trim <- function (x) gsub("^\\s+|\\s+$", "", x)
 
 predictText <- function(txt, N = 3){
+        # reset max N to 3
         if(N > 3) N <- 3
-        print(N)
-#         ptm <- proc.time()
-        txt.list   <- as.list(strsplit(trim(txt), " "))[[1]]
+
+        txt.list   <- as.list(strsplit(trim(cleanStrings(txt)), " "))[[1]]
         n <- length(txt.list)
         prediction <- quadf[as.list(txt.list)]
         prediction <- prediction[, c(1, n+2), with = FALSE]
         prediction <- setorder(prediction, -'freq')
-#         print(proc.time() - ptm)
+
         #         if(is.na(prediction$freq)){
         #                 prediction <- trif[txt.list]
         #                 if(is.na(prediction)){
@@ -23,5 +24,10 @@ predictText <- function(txt, N = 3){
         #                         }
         #                 }
         #         }
-        return(unique(prediction[1:N]))
+        return(unique(prediction))
 }
+
+print(predictText(' the obama', N = 3))
+prediction <- predictText(' the obama', N = 3)
+
+which(is.na(prediction$freq), arr.ind=TRUE)
