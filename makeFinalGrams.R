b15f35adbe2ff11ee833f5277879d659711fcef5
aousabdo <aousabdo@gmail.com>
2014-12-10 22:49:35 -0500

    increased ngram sizes

---
+++ makeFinalGrams.R
@@ -0,0 +1,26 @@
+unigrams  <- readRDS('unigrams10000.Rds')
+bigrams   <- readRDS('bigrams10000.Rds')
+trigrams  <- readRDS('trigrams10000.Rds')
+quadgrams <- readRDS('quadgrams10000.Rds')
+
+quadgrams2 <- fread('w4_.txt')
+setnames(quadgrams2, c('freq', 'w1', 'w2', 'w3', 'w4'))
+setcolorder(quadgrams2, c("w1", "w2", "w3", "w4", "freq"))
+quadgrams.all <- merge(quadgrams, quadgrams2, all = T, by=c('w1', 'w2', 'w3','w4', 'freq'))
+quadgrams.all[, prob:=freq/nrow(quadgrams.all)]
+quadgrams <- quadgrams.all
+
+fivegrams <- fread('w5_.txt')
+setnames(fivegrams, c('freq', 'w1', 'w2', 'w3', 'w4', 'w5'))
+setcolorder(fivegrams, c("w1", "w2", "w3", "w4", "w5", "freq"))
+fivegrams[, prob:=freq/nrow(fivegrams)]
+setkeyv(fivegrams, c('w1', 'w2', 'w3', 'w4', 'w5', 'freq'))
+
+setcolorder(bigrams,   c("w1", "w2", "freq", "prob"))
+setcolorder(trigrams,  c("w1", "w2", "w3", "freq", "prob"))
+setcolorder(quadgrams, c("w1", "w2", "w3", "w4", "freq", "prob"))
+setcolorder(fivegrams, c("w1", "w2", "w3", "w4", "w5", "freq", "prob"))
+
+# write files 
+saveRDS(quadgrams, 'quadgrams.Rds')
+saveRDS(fivegrams, 'fivegrams.Rds')
