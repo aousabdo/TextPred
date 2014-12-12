b5b66ddf526a437975313d643457ce7e65bcb40e
aousabdo <aousabdo@gmail.com>
2014-12-11 21:46:36 -0500

    updated ngram files

--- makeFinalGrams.R
+++ makeFinalGrams.R
@@ -3,6 +3,9 @@ bigrams   <- readRDS('bigrams10000.Rds')
 trigrams  <- readRDS('trigrams10000.Rds')
 quadgrams <- readRDS('quadgrams10000.Rds')
 
+setcolorder(bigrams, c("w1", "w2", "freq", 'prob'))
+setcolorder(trigrams, c("w1", "w2", "w3", "freq", 'prob'))
+
 quadgrams2 <- fread('w4_.txt')
 setnames(quadgrams2, c('freq', 'w1', 'w2', 'w3', 'w4'))
 setcolorder(quadgrams2, c("w1", "w2", "w3", "w4", "freq"))
@@ -22,5 +25,7 @@ setcolorder(quadgrams, c("w1", "w2", "w3", "w4", "freq", "prob"))
 setcolorder(fivegrams, c("w1", "w2", "w3", "w4", "w5", "freq", "prob"))
 
 # write files 
+saveRDS(bigrams, 'bigrams.Rds')
+saveRDS(trigrams, 'trigrams.Rds')
 saveRDS(quadgrams, 'quadgrams.Rds')
 saveRDS(fivegrams, 'fivegrams.Rds')
